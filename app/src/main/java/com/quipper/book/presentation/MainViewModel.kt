711b4d962832abffd48f99163ed9c5e08c7a6c9f
Bobby Prabowo <prabowo.bobby@gmail.com>
2020-04-21 11:36:15 +0700

    add replay to persist last state

--- app/src/main/java/com/quipper/book/presentation/MainViewModel.kt
+++ app/src/main/java/com/quipper/book/presentation/MainViewModel.kt
@@ -14,6 +14,7 @@ class MainViewModel @Inject constructor(private val useCase: GetPopularUseCase) 
 
     private val publishSubject: PublishSubject<MainState> = PublishSubject.create()
     private val compositeDisposable = CompositeDisposable()
+    private val state = publishSubject.replay(1).autoConnect(0)
 
     fun initializeData() {
         val disposable = useCase.execute()
@@ -48,7 +49,7 @@ class MainViewModel @Inject constructor(private val useCase: GetPopularUseCase) 
     }
 
     fun getState(): Observable<MainState> {
-        return publishSubject
+        return state
     }
 }
 
